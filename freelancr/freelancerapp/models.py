980f5a7de890f498db2e7942deba7dc5dfb225b0
Naichuan Zhang <nzhang@bu.edu>
2017-04-29 17:36:13 -0400

    new model

--- freelancr/freelancerapp/models.py
+++ freelancr/freelancerapp/models.py
@@ -6,9 +6,69 @@ from django.db.models import ImageField
 from phonenumber_field.modelfields import PhoneNumberField
 from django.contrib.auth import authenticate
 from django import forms
+from django.contrib.auth.models import User
 
 import os
 # Create your models here.
+<<<<<<< HEAD
+
+
+def get_image_path(instance, filename):
+    return os.path.join('photos', str(instance.id), filename)
+
+class Gender(Choices):
+    male = Choice("male")
+    female = Choice("female")
+    not_specified = Choice("not specified")
+
+class ProfileInfo(models.Model):
+	user = models.OneToOneField(User, related_name='profileinfo', on_delete = models.CASCADE)
+    image1 = ImageField(upload_to="media", blank=True, null=True)
+    image2 = ImageField(upload_to="media", blank=True, null=True)
+    image3 = ImageField(upload_to="media", blank=True, null=True)
+    #False -- talent, True -- hire
+    identity = BooleanField(default = False)
+    # Freelancer field
+    firstName = models.CharField(
+        max_length=50,
+        default="",
+        validators=[RegexValidator(regex='[a-zA-Z]$', message='Only letters can be input')],
+        primary_key = True
+    )
+    middleInitial = models.CharField(
+        max_length=1,
+        default="",
+        validators=[RegexValidator(regex='[a-zA-Z]$', message='Only letters can be input')],
+        null = True,
+    )
+    lastName = models.CharField(
+        max_length=50,
+        default="",
+        validators=[RegexValidator(regex='[a-zA-Z]$', message='Only letters can be input')],
+        primary_key=True
+    )
+    gender = Gender.from_id(self.gender)
+    # Company field
+    name = models.CharField(
+        max_length=50,
+        default="",
+        primary_key=True
+    )
+    details = models.CharField(
+        max_length=500,
+        default="",
+        null = True
+    )
+    # Common field
+    phoneNumber = PhoneNumberField(primary_key=True)
+    email = models.EmailField(default="",primary_key=True)
+
+class Skill(models.Model):
+    skillName = Textfield(primary_key = True)
+
+'''
+=======
+>>>>>>> 1b375eed7744b0d194490ec907880d0433c9faaf
 class Gender(Choices):
     male = Choice("male")
     female = Choice("female")
@@ -73,5 +133,4 @@ class Company(models.Model):
         null = True
     )
 
-
-
+'''
